2284c2748d035388e32417e56fa79fae4c9ac26b
Serg Baburin <serg.baburin@gmail.com>
2015-04-30 15:06:35 +0300

    Fix detection virtualenvwrapper. Auto updating oh-my-zsh.

--- install.sh
+++ install.sh
@@ -33,14 +33,20 @@ if [ "$unamestr" == "Darwin" ]; then
   fi
 
   echo "Check virtualenvwrapper..."
-  which virtualenvwrapper > /dev/null
+  which virtualenvwrapper.sh > /dev/null
   if [ "$?" != "0" ]; then
     echo "Oops... 'virtualenvwrapper' was not found, install it now"
-    sudo pip install virtualenvwrapper
+    if [ -f /usr/local/bin/python ]; then
+      echo "Hmm.. Python looks like homebrewed, \
+are trying to install pip without root permissions."
+      pip install virtualenvwrapper
+    else
+      sudo pip install virtualenvwrapper
+    fi
   fi
 
 else
-  echo "TODO: Add ubuntu checker"
+  echo "TODO: Add linux checker"
 fi
 
 
@@ -61,23 +67,32 @@ for dir in */; do
   fi
 done
 
-cd ~
+
 if [ ! -d ~/.oh-my-zsh ]; then
+  cd ~
   curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+else
+  echo "Update oh-my-zsh"
+  cd ~/.oh-my-zsh
+  git pull --rebase --stat origin master
 fi
+
 
 if [ ! -d ~/.ve ]; then
   mkdir ~/.ve
 fi
 
+
 if [ ! -d ~/.commshots ]; then
   ln -s ~/Dropbox/commshots ~/.commshots
 fi
 
-if [! -d ~/.atom ]; then
+
+if [ ! -d ~/.atom ]; then
   ln -s ~/Dropbox/Apps/.atom ~/.atom
 fi
 
-if [! -d ~/Library/Application\ Support/Sublime\ Text\ 3 ]; then
+
+if [ ! -d ~/Library/Application\ Support/Sublime\ Text\ 3 ]; then
   ln -s ~/Dropbox/Apps/Sublime\ Text\ 3 ~/Library/Application\ Support/Sublime\ Text\ 3
 fi
