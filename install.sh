8ebbe1fb30b77f6aff979b0848b17018849d8d4c
Serg Baburin <serg.baburin@gmail.com>
2019-03-03 14:04:04 +0300

    Remove copy settings and aliases from Dropbox

--- install.sh
+++ install.sh
@@ -77,15 +77,5 @@ fi
 
 
 if [ ! -d ~/.commshots ]; then
-  ln -s ~/Dropbox/commshots ~/.commshots
-fi
-
-
-if [ ! -d ~/.atom ]; then
-  ln -s ~/Dropbox/Apps/.atom ~/.atom
-fi
-
-
-if [ ! -d ~/Library/Application\ Support/Sublime\ Text\ 3 ]; then
-  ln -s ~/Dropbox/Apps/Sublime\ Text\ 3 ~/Library/Application\ Support/Sublime\ Text\ 3
+  mkdir ~/.commshots
 fi
